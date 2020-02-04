55bf0a486f21c424bf3de434ed17e521805e34ba
lomeat <lom3at@gmail.com>
2020-02-04 22:38:36 +0300

    change view of time select modal

--- src/body/time-select-list.js
+++ src/body/time-select-list.js
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 export const TimeSelectList = ({ time, timeType, handleItemClick }) => {
-  const currentTimeType = timeType === "hours" ? time.hours : time.minutes;
+  const currentTimeArr = timeType === "hours" ? time.hours : time.minutes;
 
-  const content = currentTimeType.map(timeEl => (
+  const content = currentTimeArr.map(timeEl => (
     <Item onClick={() => handleItemClick(timeEl)}>
       {timeEl < 10 ? "0" + timeEl : timeEl}
     </Item>
@@ -12,10 +12,17 @@ export const TimeSelectList = ({ time, timeType, handleItemClick }) => {
 
   return (
     <ListWrapper>
-      <List>{content}</List>
+      <Grid timeType={timeType}>{content}</Grid>
     </ListWrapper>
   );
 };
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: ${props =>
+    props.timeType === "hours" ? "repeat(8, 1fr)" : "repeat(6, 1fr)"};
+  grid-gap: 20px;
+`;
 
 const ListWrapper = styled.div`
   background: #f15a51;
@@ -24,25 +31,16 @@ const ListWrapper = styled.div`
   position: absolute;
   display: flex;
   justify-content: center;
+  align-items: center;
   z-index: 2;
   top: 0;
   left: 0;
 `;
 
-const List = styled.div`
-  background: #f15a51;
-  color: #ffe3e1;
-  font-size: 140px;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  overflow: hidden;
-  overflow-y: scroll;
-`;
-
 const Item = styled.div`
   padding: 10px;
   text-align: center;
+  font-size: 120px;
   &:hover {
     cursor: pointer;
     color: #ccc;
